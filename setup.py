1fa22c3ecc048878bf4aa0409b047849cb76d109
Robin van der Noord <robinvandernoord@gmail.com>
2022-07-26 13:45:16 +0200

    bump(patch): 0.1.4

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(os.path.join(os.getcwd(), "requirements-dev.txt")) as f:
 
 setup(
     name="edwh-ghost",
-    version="0.1.3",
+    version="0.1.4",
     description="Python client for Ghost API v3/v4/v5",
     url="https://github.com/educationwarehouse/edwh-ghost",
     author="Education Warehouse",
